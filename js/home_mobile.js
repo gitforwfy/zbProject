36b6b1abee483a0fb0989c935a32d7cc6078c503
gitforwfy <fly940912@sina.com>
2018-10-16 18:53:26 +0800

    add some items as

--- js/home_mobile.js
+++ js/home_mobile.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     // $(".h1_user_info").innerHTML= "用户id：" + sessionStorage.getItem("uid") + "\n"
     //     + "手机号:" + sessionStorage.getItem("phone");
 
-    document.getElementById("h1_user_info").innerHTML= "欢迎"+ sessionStorage.getItem("phone")+"会员";
+    document.getElementById("h1_user_info").innerHTML= "欢迎"+ sessionStorage.getItem("phone")+" 普通会员";
     // $(".h1_user_info").innerHTML= "欢迎" + "手机号:" + sessionStorage.getItem("phone")+"会员";
 });
