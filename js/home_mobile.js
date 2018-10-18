1729533c04123d0d03d45299c7cf9c98dc1922f1
gitforwfy <fly940912@sina.com>
2018-10-18 18:59:37 +0800

    add help gethelp

--- js/home_mobile.js
+++ js/home_mobile.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
     // $(".h1_user_info").innerHTML= "用户id：" + sessionStorage.getItem("uid") + "\n"
     //     + "手机号:" + sessionStorage.getItem("phone");
+    var hasLogin=false;
+    var phone=sessionStorage.getItem("phone");
+    if(phone==null){
+        hasLogin=false;
+    }else{
+        hasLogin=true;
+    }
 
-    document.getElementById("h1_user_info").innerHTML= "欢迎"+ sessionStorage.getItem("phone")+" 普通会员";
-    // $(".h1_user_info").innerHTML= "欢迎" + "手机号:" + sessionStorage.getItem("phone")+"会员";
+    if(hasLogin){
+        document.getElementById("h1_user_info").innerHTML= "欢迎"+ sessionStorage.getItem("phone")+" 普通会员";
+    }else{
+        window.location.href="login_mobile.html";
+    }
 });
